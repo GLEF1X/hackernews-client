f36d923ad48690075158189891352633d1e04f51
GLEF1X <glebgar567@gmail.com>
2023-05-27 13:08:35 -0400

    add some styles for layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,12 @@
 import { Layout } from "antd";
 import { Content } from "antd/es/layout/layout";
 import { Outlet } from "react-router-dom";
+import styles from "./Layout.module.css";
 
 export default function RootLayout() {
   return (
     <Layout style={{ padding: "10px", backgroundColor: "white" }}>
-      <Content style={{ margin: "0 250px" }}>
+      <Content className={styles.content}>
         <Outlet />
       </Content>
     </Layout>
