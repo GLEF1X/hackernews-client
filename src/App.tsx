94012cd953509be89f92a43ee503ba2dcbc1c1bd
GLEF1X <glebgar567@gmail.com>
2023-05-26 21:12:25 -0400

    Implement cache persistence to maintain data even after the page is refreshed.

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,27 @@ import { ConfigProvider } from "antd";
 import ukUA from "antd/locale/uk_UA";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
-import { QueryClientProvider } from "@tanstack/react-query";
 import { ApiClientProvider } from "./services/api/api-client";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import queryClient from "./services/query-client";
-import * as React from "react";
+import queryClient, { CACHE_TIME_IN_MILLISECONDS, persister } from "./services/query-client";
 import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
-import Spinner from "./components/Spinner/Spinner";
+import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
+import "./App.css";
 
 function App() {
   return (
     <ErrorBoundary>
-      <React.Suspense fallback={<Spinner />}>
-        <ConfigProvider locale={ukUA}>
-          <QueryClientProvider client={queryClient}>
-            <ReactQueryDevtools initialIsOpen={false} />
-            <ApiClientProvider>
-              <RouterProvider router={router} />
-            </ApiClientProvider>
-          </QueryClientProvider>
-        </ConfigProvider>
-      </React.Suspense>
+      <ConfigProvider locale={ukUA}>
+        <PersistQueryClientProvider
+          client={queryClient}
+          persistOptions={{ persister, maxAge: CACHE_TIME_IN_MILLISECONDS }}
+        >
+          <ReactQueryDevtools initialIsOpen={false} />
+          <ApiClientProvider>
+            <RouterProvider router={router} />
+          </ApiClientProvider>
+        </PersistQueryClientProvider>
+      </ConfigProvider>
     </ErrorBoundary>
   );
 }
--- src/services/query-client.ts
+++ src/services/query-client.ts
@@ -1,10 +1,13 @@
 import { QueryClient } from "@tanstack/react-query";
+import { createSyncStoragePersister } from "@tanstack/query-sync-storage-persister";
+
+export const CACHE_TIME_IN_MILLISECONDS = 1000 * 60; // 1 minute
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      cacheTime: 1000 * 60,
-      staleTime: 1000 * 60, // 1 minute
+      cacheTime: CACHE_TIME_IN_MILLISECONDS,
+      staleTime: CACHE_TIME_IN_MILLISECONDS,
       retry: 0,
       refetchOnWindowFocus: true,
       refetchOnMount: false,
@@ -12,6 +15,10 @@ const queryClient = new QueryClient({
       suspense: true,
     },
   },
+});
+
+export const persister = createSyncStoragePersister({
+  storage: window.localStorage,
 });
 
 export default queryClient;
