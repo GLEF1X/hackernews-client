aea14b8c4cbb0338389c185bc4af728e5c4a0b7d
GLEF1X <glebgar567@gmail.com>
2023-05-28 11:09:40 -0400

    Make spinner centered

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -4,12 +4,11 @@ import { LoadingOutlined } from "@ant-design/icons";
 
 export default function Spinner() {
   return (
-    <div className={classes.spinnerWrapper}>
-      <Spin
-        indicator={<LoadingOutlined style={{ fontSize: 48 }} spin />}
-        size="large"
-        tip="Loading..."
-      />
-    </div>
+    <Spin
+      indicator={<LoadingOutlined style={{ fontSize: 48 }} spin />}
+      className={classes.spinnerWrapper}
+      size="large"
+      tip="Loading..."
+    />
   );
 }
